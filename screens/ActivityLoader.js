c59b08b3676108e70605725bac566710a00380c1
dashingshree6 <dashingshree6@gmail.com>
2023-09-14 15:27:58 +0530

    Update ActivityLoader.js

--- screens/ActivityLoader.js
+++ screens/ActivityLoader.js
@@ -8,14 +8,10 @@ export default function ActivityLoader() {
       zIndex:9000,
       alignItems:'center',
       justifyContent:'center',
-    //   position:"absolute",
-    //   top:0,
-    //   left:0,
       width:'100%',
       height:"100%",
       backgroundColor:'silver',
       opacity:0.5,
-    //   flex:1
     }}
     >
     <ActivityIndicator size="large" color="red" /><Text>Loading</Text>
