fad98082d0abfc3f6aea04d495c1252290c0f65e
dashingshree6 <dashingshree6@gmail.com>
2023-09-14 15:24:31 +0530

    Update Login.js

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native';
 import { signinApi } from '../../API';
 import SyncStorage from 'sync-storage';
-import { AppContext } from '../../App';
+import { useSelector, useDispatch } from 'react-redux'
+import { loaderOff, loaderOn, login } from '../Redux/authenticationSlice';
 
 const LoginScreen = ({ navigation }) => {
-  const {dispatch} = React.useContext(AppContext);
+  const dispatch = useDispatch()
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -15,36 +16,103 @@ const LoginScreen = ({ navigation }) => {
         email: email,
         password: password
     }
+    dispatch(loaderOn())
 
     signinApi(body).then((res)=>{
         SyncStorage.set('userDetails', res.data);
         SyncStorage.set('token', res?.data?.token);
-        dispatch({type: 'login'});
-
+        dispatch(login()) 
+        dispatch(loaderOff())
         navigation.navigate('Home');
     }).catch(err =>{
+        dispatch(loaderOff())
         console.log(err)
     })
   };
 
   return (
-    <View>
-      <Text>Email:</Text>
-      <TextInput
-        onChangeText={setEmail}
-        value={email}
-        placeholder="Enter your email"
-      />
-      <Text>Password:</Text>
-      <TextInput
-        onChangeText={setPassword}
-        value={password}
-        secureTextEntry
-        placeholder="Enter your password"
-      />
-      <Button title="Login" onPress={handleLogin} />
+    <View style={styles.mainContainer}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.welcomeText}>Welcome back!</Text>
+        <Text style={styles.loginText}>Login to your existing account</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.label}>Email:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setEmail}
+          value={email}
+          placeholder="Enter your email"
+        />
+
+        <Text style={styles.label}>Password:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+          placeholder="Enter your password"
+        />
+
+        <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={handleLogin}
+        >
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center', // Center the content vertically
+    padding:10
+  },
+  container: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Semi-transparent white background
+    padding: 20,
+    borderRadius: 10,
+  },
+  label: {
+    fontSize: 12,
+    marginBottom: 4,
+    color: 'grey',
+  },
+  headerContainer: {
+    alignItems:'center',
+    marginBottom:20
+  },
+  welcomeText:{
+    fontSize:30,
+    fontWeight:'bold',
+    color:'black'
+  },
+  loginText:{
+    fontSize:10,
+    fontWeight:'bold',
+    color:'grey'
+  },
+  input: {
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 24,
+    padding: 8,
+    marginBottom: 12,
+    backgroundColor: 'white',
+  },
+  loginBtn: {
+    alignItems:'center',
+    padding:12,
+    backgroundColor:'#ffcc00',
+    borderRadius:24
+  },
+  loginText: {
+    color:'grey'
+  }
+});
 
 export default LoginScreen;
